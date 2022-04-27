0e18119fac3a207218b217d290b692704b4d7a2f
Uilord_F.Gray <82732181+ar4angeel@users.noreply.github.com>
2022-04-27 17:12:26 +0300

    Delete main_db.py

--- SQLlite3/main_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-con = sqlite3.connect('vprok_borsch_part.db')
-cur = con.cursor()
-with con:
-    cur.execute("CREATE TABLE IF NOT EXISTS 'vprok_borsch_part' ('ID' INTEGER, 'AMOUNT' REAL, 'PRICE' REAL)")
-    con.commit()
-    cur.close()
