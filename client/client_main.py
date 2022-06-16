df0c128a44f2c7b16bf0f26e7aed99e3803fb479
Главный <likeabro1@gmail.com>
2022-06-16 21:30:18 +0300

    ---

--- client/client_main.py
+++ client/client_main.py
@@ -1,11 +1,20 @@
 from flask import Flask, url_for, render_template
-from models import (Product, Price)
+from models import (Product, Price, db)
 app = Flask(__name__)
 
-@app.route('/<id>')
+@app.before_request
+def before_request():
+    db.connect()
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
+
+@app.route('/id/<id>')
 def get_product_from_id(id):
-    a = Product.get_by_id(int(id)).name
-    return '<h1>%s</h1>' % a
+    a = Product.get_by_id(id).name
+    return a
 
 if __name__ == '__main__':
     app.run(debug=True)
